04b4a830db49d7650710386eee4ae298006c846b
keanuk <keanu@akua.com>
2018-04-02 04:07:29 -0400

    Working on displaying FB data

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,6 +1,18 @@
 (function () {
   'use strict';
 
+  let at = 'EAACEdEose0cBABZCvjBqpreDZBOmkkKpiuZBwlFq8gTLoZAg7FdDhJSu46hwkqQOlApPZB31OqQmnkz2nXyVzINrDRbqpgbQ2pLegXo3tVbOAJG7zOMWPGeJPJUnpTlrIJz6kjVi4WOPx17smRDqZCgI45UUy8xX5Hp3l8hFY0CR7z0GfLsf421f4qXS37CY52UjZBvmd5u23bqXSKnOovQ';
+
+  let alachuaHumaneEvents = [];
+
+  // getAlachuaHumane();
+  // getAARP();
+  // getSustainableUF();
+  // getShands();
+  // getHarn();
+  // getGatorSmiles();
+  // getGatorPals();
+
   angular
     .module('core')
     .controller('HomeController', HomeController);
@@ -10,57 +22,131 @@
   function HomeController($scope, $state, Authentication) {
     var vm = this;
     console.log(Authentication);
+
+    $scope.$on('$viewContentLoaded', function(){
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '291380158062880',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.12'
+      });
+
+      // FB.AppEvents.logPageView();
+
+      FB.getLoginStatus(function(response) {
+          statusChangeCallback(response);
+      });
+
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
+     function statusChangeCallback(response) {
+       if(response.status === 'connected') {
+         console.log('Logged in and authenticated');
+         console.log(response);
+         // at = response.authResponse.accessToken;
+         getAlachuaHumane($scope);
+         getAARP();
+         getSustainableUF();
+         getShands();
+         getHarn();
+         getGatorSmiles();
+         getGatorPals();
+       }
+       else {
+         console.log('Not authenticated');
+       }
+     }
+   });
   }
+
+
+   function getAlachuaHumane($scope) {
+     FB.api('/' + 'alachuahumane.org?fields=name,events' + '&access_token=' + at, function(response) {
+       if(response && !response.error) {
+         console.log(response.name);
+         $scope.alachuaHumane = response.name;
+         $scope.alachuaHumaneEvents = response.events.data[0];
+       }
+       else {
+         console.log(response.error);
+       }
+     })
+   }
+
+   function getAARP() {
+     FB.api('/' + 'AARP?fields=name,events' + '&access_token=' + at, function(response) {
+       if(response && !response.error) {
+         console.log(response);
+       }
+       else {
+         console.log(response.error);
+       }
+     })
+   }
+
+   function getSustainableUF() {
+     FB.api('/' + 'sustainableuf?fields=name,events' + '&access_token=' + at, function(response) {
+       if(response && !response.error) {
+         console.log(response);
+       }
+       else {
+         console.log(response.error);
+       }
+     })
+  }
+
+  function getShands() {
+    FB.api('/' + 'ShandsArtsinMedicine?fields=name,events' + '&access_token=' + at, function(response) {
+      if(response && !response.error) {
+        console.log(response);
+      }
+      else {
+        console.log(response.error);
+      }
+    })
+  }
+
+  function getHarn() {
+    FB.api('/' + 'HarnMuseumofArt?fields=name,events' + '&access_token=' + at, function(response) {
+      if(response && !response.error) {
+        console.log(response);
+      }
+      else {
+        console.log(response.error);
+      }
+    })
+  }
+
+  function getGatorSmiles() {
+    FB.api('/' + 'gatorsmiles?fields=name,events' + '&access_token=' + at, function(response) {
+      if(response && !response.error) {
+        console.log(response);
+      }
+      else {
+        console.log(response.error);
+      }
+    })
+  }
+
+  function getGatorPals() {
+    FB.api('/' + 'gatorpalsapp2017?fields=name,events' + '&access_token=' + at, function(response) {
+      if(response && !response.error) {
+        console.log(response);
+      }
+      else {
+        console.log(response.error);
+      }
+    })
+  }
+
 }());
-
-// Facebook API calls have been disabled temporarily
-
-let at = 'EAACEdEose0cBAIaOorkydG0LgWJ5g5aUypJpsNEp4NAYOGuN9JowRObGZAc0QTZCJH0ixTzaRrD4mPT8BzZBJflrdkl2MNrZAiviIpFigBlL59KPt4tbfd1c5TlyfeXFZB4fyKSSTyvtjGX0579TqLzoYZBdUXct246ZAw4imS1vjLcl5m0zbjS9YZArZBV2pGdBhLZCchxCjNsy83zqeXn7it';
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '291380158062880',
-    cookie     : true,
-    xfbml      : true,
-    version    : 'v2.12'
-  });
-
-  // FB.AppEvents.logPageView();
-
-  FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-  });
-
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
- function statusChangeCallback(response) {
-   if(response.status === 'connected') {
-     console.log('Logged in and authenticated');
-     console.log(response);
-     // at = response.authResponse.accessToken;
-     fbCall();
-   }
-   else {
-     console.log('Not authenticated');
-   }
- }
-
- function fbCall() {
-   console.log(at);
-   FB.api('/' + 'alachuahumane.org?fields=name,events' + '&access_token=' + at, function(response) {
-     if(response && !response.error) {
-       console.log(response);
-     }
-     else {
-       console.log(response.error);
-     }
-   })
- }
